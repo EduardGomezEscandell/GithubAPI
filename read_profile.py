db0df92ad7cca60240630c1933a8cfd80572deb8
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-07-04 11:38:40 +0200

    Hotfix

--- read_profile.py
+++ read_profile.py
@@ -6,4 +6,5 @@ username = "EduardGomezEscandell"
 
 cmd = f'curl https://api.github.com/users/{username}'
 result = requests.get(f'https://api.github.com/users/{username}', headers={'Accept': 'application/vnd.github.v3+json'})
+result.raise_for_status()
 print(json.dumps(json.loads(result.text), indent=2))
